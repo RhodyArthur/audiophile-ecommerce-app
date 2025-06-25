98fc85a8efa9f59348591e151345e9dd978b20b1
RhodyArthur <rhodaarthur48@gmail.com>
2025-06-25 09:43:42 +0000

    feat: add a reusable button component

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../header/header";
+import { Button } from "../../shared/button/button";
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [Button],
   templateUrl: './hero.html',
   styleUrl: './hero.sass'
 })
