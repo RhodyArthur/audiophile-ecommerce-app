fdb6e68e7893005391c06a9e57bfe6b596dae558
RhodyArthur <rhodaarthur48@gmail.com>
2025-06-25 15:20:51 +0000

    feat: add footer compoent layout and styles

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from "../../components/hero/hero";
 import { About } from "../../components/about/about";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, About],
+  imports: [Hero, About, Footer],
   templateUrl: './home.html',
   styleUrl: './home.sass'
 })
