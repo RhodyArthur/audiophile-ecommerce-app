68edb1864f50341bf81e455875b4f0b26622b83d
RhodyArthur <rhodaarthur48@gmail.com>
2025-06-26 15:34:12 +0000

    feat: add login route to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Footer } from './components/footer/footer';
+import { Login } from './components/auth/login/login';
 
 export const routes: Routes = [
     {path: '', component: Home},
+
+    {
+        path: 'login',
+        loadComponent: () => import('../app/components/auth/login/login').then(m => m.Login)
+    },
     {path: '**', component: Footer}
 ];
