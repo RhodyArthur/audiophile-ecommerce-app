ceefcf2fe381cd321dae4ef8b55194458051f810
RhodyArthur <rhodaarthur48@gmail.com>
2025-06-27 12:02:53 +0000

    feat: add form validations to login component

--- src/app/components/auth/login/login.ts
+++ src/app/components/auth/login/login.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.sass'
 })
 export class Login {
+
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  loginFom: FormGroup = this.fb.group({
+       email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  })
+
+  submit() {
+    if(this.loginFom.valid) {
+      const formData = this.loginFom.value
+      console.log(formData)
+      this.router.navigate(['/'])
+    }
+  }
+
 
 }
