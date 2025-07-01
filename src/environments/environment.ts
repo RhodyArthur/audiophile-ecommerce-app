ac9f3fa267dd420d38abb25ff0685f324ec4c6b6
RhodyArthur <rhodaarthur48@gmail.com>
2025-07-01 13:17:27 +0000

    feat: migrate data.json to supabase

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    supabaseUrl: '${SUPABASE_URL}',
+    supabaseKey: '${SUPABASE_KEY}',
+    // supabaseUrl: 'https://qqcrbnktidcgljpvtkjg.supabase.co',
+    // supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxY3Jibmt0aWRjZ2xqcHZ0a2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyOTE0NzUsImV4cCI6MjA2Njg2NzQ3NX0.4ANemiWjvr0kUZQQyqN03sMmIOfiSVihFsgxy4n1YgM'
+};
