da423e21dc439b96a5ee80553da6310e52a152b0
RhodyArthur <rhodaarthur48@gmail.com>
2025-07-02 07:37:18 +0000

    Stop tracking environment.development.ts

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    supabaseUrl: 'https://qqcrbnktidcgljpvtkjg.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxY3Jibmt0aWRjZ2xqcHZ0a2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyOTE0NzUsImV4cCI6MjA2Njg2NzQ3NX0.4ANemiWjvr0kUZQQyqN03sMmIOfiSVihFsgxy4n1YgM'
-};
