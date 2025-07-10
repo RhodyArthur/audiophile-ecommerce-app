1544f5466687cc17f4039e71a7081d2f362de7c4
RhodyArthur <rhodaarthur48@gmail.com>
2025-07-10 08:09:31 +0000

    add navigation to detail page on landing page

--- src/app/components/home-products/home-products.ts
+++ src/app/components/home-products/home-products.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Speakerx9Card } from "./speakerx9-card/speakerx9-card";
 import { Button } from "../../shared/button/button";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-products',
@@ -9,5 +10,15 @@ import { Button } from "../../shared/button/button";
   styleUrl: './home-products.sass'
 })
 export class HomeProducts {
-
+  private router = inject(Router);
+  speaker = signal<string>('zx7-speaker');
+  earphones = signal<string>('yx1-earphones');
+  
+    viewSpeakerDetails() {
+      this.router.navigate(['product-details', this.speaker()])
+    }
+  
+    viewEarphoneDetails() {
+      this.router.navigate(['product-details', this.earphones()])
+    }
 }
