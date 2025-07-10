8b9f4900b4dbb8b60cb2bfe04011994a4827a1b4
RhodyArthur <rhodaarthur48@gmail.com>
2025-07-10 14:22:31 +0000

    feat: complete form validators

--- src/app/components/checkout-form/checkout-form.ts
+++ src/app/components/checkout-form/checkout-form.ts
@@ -1,6 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { phoneNumberValidator } from '../../shared/validators/phoneNumber.validator';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-checkout-form',
@@ -10,6 +13,16 @@ import { phoneNumberValidator } from '../../shared/validators/phoneNumber.valida
 })
 export class CheckoutForm {
   private fb = inject(FormBuilder);
+  isVirtual = signal<boolean>(true);
+  private destroy$ = new Subject<void>();
+
+  ngOnInit() {
+    this.checkoutForm.get('payment.method')?.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(value => {
+        this.isVirtual.set(value === 'e-money');
+      });
+  }
 
   checkoutForm: FormGroup = this.fb.group({
     billingDetails: this.fb.group({
@@ -17,5 +30,30 @@ export class CheckoutForm {
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required, phoneNumberValidator()]]
     }),
+
+    shippingInfo: this.fb.group({
+      address: ['' , Validators.required],
+      zipCode: ['', [Validators.required, Validators.minLength(5)]],
+      city: ['', Validators.required],
+      country: ['', Validators.required]
+    }),
+
+    payment: this.fb.group({
+    method: ['e-money', Validators.required],
+    eMoneyNumber: ['', [Validators.required, Validators.minLength(9)]],
+    eMoneyPin: ['', [Validators.required, Validators.minLength(4)]],
   })
+  })
+
+  submit() {
+    if (this.checkoutForm.valid) {
+      console.log(this.checkoutForm.value)
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 }
