b2a6ff6cee15a3fdedf818099071399f5241a643
RhodyArthur <rhodaarthur48@gmail.com>
2025-07-17 15:30:51 +0000

    fix: add login link to mobile-menu-modal

--- src/app/components/modals/mobile-menu-modal/mobile-menu-modal.ts
+++ src/app/components/modals/mobile-menu-modal/mobile-menu-modal.ts
@@ -1,15 +1,16 @@
 import { Component, inject, output } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth-service';
 
 @Component({
   selector: 'app-mobile-menu-modal',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './mobile-menu-modal.html',
   styleUrl: './mobile-menu-modal.sass'
 })
 export class MobileMenuModal {
   private router = inject(Router);
-  private route = inject(ActivatedRoute);
+  protected authService = inject(AuthService);
   close = output<void>();
 
   ngOnInit() {
